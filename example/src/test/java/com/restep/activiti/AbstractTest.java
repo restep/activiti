54319628c3cf86c15e8a9e5a6a2c3d18a0b4e58c
restep <restep@163.com>
2018-12-22 00:21:27 +0800

    helloworld

---
+++ example/src/test/java/com/restep/activiti/AbstractTest.java
@@ -0,0 +1,43 @@
+package com.restep.activiti;
+
+import org.activiti.engine.*;
+import org.junit.Before;
+
+/**
+ * @author restep
+ * @date 2018/12/21
+ */
+public class AbstractTest {
+    protected RepositoryService repositoryService;
+    protected RuntimeService runtimeService;
+    protected TaskService taskService;
+    protected HistoryService historyService;
+    protected IdentityService identityService;
+    protected ManagementService managementService;
+    protected FormService formService;
+
+    @Before
+    public void setUp() {
+        ProcessEngineConfiguration configuration = ProcessEngineConfiguration.createStandaloneProcessEngineConfiguration();
+        configuration.setJdbcDriver("com.mysql.jdbc.Driver");
+        configuration.setJdbcUrl("jdbc:mysql://localhost:3306/activiti?useUnicode=true&characterEncoding=UTF-8");
+        configuration.setJdbcUsername("root");
+        configuration.setJdbcPassword("root");
+
+        /**
+         * DB_SCHEMA_UPDATE_FALSE 不能自动创建表
+         * DB_SCHEMA_UPDATE_CREATE_DROP 先删除表再创建表
+         * DB_SCHEMA_UPDATE_TRUE 如何表不存在 自动创建和更新表
+         */
+        configuration.setDatabaseSchemaUpdate(ProcessEngineConfiguration.DB_SCHEMA_UPDATE_TRUE);
+
+        ProcessEngine processEngine = configuration.buildProcessEngine();
+        repositoryService = processEngine.getRepositoryService();
+        runtimeService = processEngine.getRuntimeService();
+        taskService = processEngine.getTaskService();
+        historyService = processEngine.getHistoryService();
+        identityService = processEngine.getIdentityService();
+        managementService = processEngine.getManagementService();
+        formService = processEngine.getFormService();
+    }
+}
--- example/src/test/java/com/restep/ch02/VerySimpleLeaveProcessTest.java
+++ example/src/test/java/com/restep/ch02/VerySimpleLeaveProcessTest.java
@@ -1,9 +1,6 @@
 package com.restep.ch02;
 
-import org.activiti.engine.ProcessEngine;
-import org.activiti.engine.ProcessEngineConfiguration;
-import org.activiti.engine.RepositoryService;
-import org.activiti.engine.RuntimeService;
+import com.restep.activiti.AbstractTest;
 import org.activiti.engine.repository.ProcessDefinition;
 import org.activiti.engine.runtime.ProcessInstance;
 import org.junit.Assert;
@@ -13,22 +10,17 @@ import org.junit.Test;
  * @author restep
  * @date 2018/12/21
  */
-public class VerySimpleLeaveProcessTest {
+public class VerySimpleLeaveProcessTest extends AbstractTest {
     @Test
     public void startProcessTest() throws Exception {
-        //创建流程引擎
-        ProcessEngine processEngine = ProcessEngineConfiguration.createStandaloneInMemProcessEngineConfiguration().buildProcessEngine();
-
         //部署流程定义文件
-        RepositoryService repositoryService = processEngine.getRepositoryService();
-        repositoryService.createDeployment().addClasspathResource("helloworld/sayhelloleave.bpmn").deploy();
+        repositoryService.createDeployment().addClasspathResource("ch02/helloworld/sayhelloleave.bpmn").deploy();
 
         //验证已部署流程定义
         ProcessDefinition processDefinition = repositoryService.createProcessDefinitionQuery().singleResult();
         Assert.assertNotNull(processDefinition);
 
         //启动流程并返回流程实例
-        RuntimeService runtimeService = processEngine.getRuntimeService();
         ProcessInstance processInstance = runtimeService.startProcessInstanceByKey("leavesayhello");
         Assert.assertNotNull(processInstance);
     }
